017cab4665a766733ca7154864d47d5576ae23af
wesselfr <wessel.frijters@gmail.com>
2021-11-08 11:35:09 +0100

    Changed copyfiles to include correct directory

--- copyfiles.bat
+++ copyfiles.bat
@@ -4,8 +4,8 @@ cd ..
 xcopy Engine\Assets\* bin\x64\Debug\Assets\* /S /Y /R /C /E
 xcopy Engine\Assets\* bin\x64\Release\Assets\* /S /Y /R /C /E
 
-xcopy Game\Game\Assets\* bin\x64\Debug\Assets\* /S /Y /R /C /E
-xcopy Game\Game\Assets\* bin\x64\Release\Assets\* /S /Y /R /C /E
+xcopy Game\Assets\* bin\x64\Debug\Assets\* /S /Y /R /C /E
+xcopy Game\Assets\* bin\x64\Release\Assets\* /S /Y /R /C /E
 
 xcopy Engine\Dependencies\*.dll bin\x64\Debug /D /S /Y /R
 xcopy Engine\Dependencies\*.dll bin\x64\Release /D /S /Y /R
